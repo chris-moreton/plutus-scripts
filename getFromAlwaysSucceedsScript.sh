3d2163973267831175e136c5a0ae82386c09dc16
Chris Moreton <chris@netsensia.com>
2021-07-28 21:44:43 +0000

    Fix fees for getFromAlwaysSucceedsScript.sh

--- getFromAlwaysSucceedsScript.sh
+++ getFromAlwaysSucceedsScript.sh
@@ -1,1 +1,1 @@
-./getFromScript.sh 10000000 200000 AlwaysSucceeds 42
+./getFromScript.sh 10000000 110174521 AlwaysSucceeds 6666
--- getFromScript.sh
+++ getFromScript.sh
@@ -7,7 +7,6 @@ DATUM_HASH=$(cardano-cli transaction hash-script-data --script-data-value $DATUM
 SCRIPT_ADDRESS=$(cardano-cli address build --payment-script-file $SCRIPT_FILE --testnet-magic 7)
 echo $SCRIPT_ADDRESS > ./wallets/${SCRIPT_NAME}.addr
 SLOT=$(./currentSlot.sh)
-CHANGE="$(($BALANCE-$PAYMENT-$FEE))"
 TTL_PLUS="$(($SLOT+30))"
 
 echo "============================================================================================"
@@ -16,14 +15,16 @@ echo "==========================================================================
 source ./functions.sh
 getInputTx ${SCRIPT_NAME}
 SCRIPT_UTXO=$INPUT_UTXO
+CHANGE="$(($TX_BALANCE-$PAYMENT-$FEE))"
 
 echo "============================================================================================"
-echo "Select Collateral UTxO"
+echo "Select Collateral UTxO - output will also be sent to this wallet"
 echo "============================================================================================"
 source ./functions.sh
 getInputTx
 COLLATERAL_TX=$INPUT_UTXO
 TO_ADDR=$TX_WALLET_ADDR
+SIGNING_WALLET=$TX_WALLET_NAME
 
 $CARDANO_CLI query protocol-parameters --testnet-magic 7 > params.json
 
@@ -39,7 +40,7 @@ $CARDANO_CLI transaction build-raw \
 --tx-in-collateral=${COLLATERAL_TX} \
 --tx-out ${TO_ADDR}+${PAYMENT} \
 --protocol-params-file "params.json" \
---tx-out ${SCRIPT_ADDR}+${CHANGE} \
+--tx-out ${SCRIPT_ADDRESS}+${CHANGE} \
 --ttl ${TTL_PLUS} \
 --fee ${FEE} \
 --out-file tx.raw \
@@ -47,7 +48,7 @@ $CARDANO_CLI transaction build-raw \
 
 $CARDANO_CLI transaction sign \
 --tx-body-file tx.raw \
---signing-key-file ~/wallets/${10}.skey \
+--signing-key-file ./wallets/${SIGNING_WALLET}.skey \
 --testnet-magic 7 \
 --out-file tx.signed \
 
