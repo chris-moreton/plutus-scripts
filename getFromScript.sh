404d92818930f8918dd82a631d15fbf4b9f5e3e9
Chris Moreton <chris@netsensia.com>
2021-08-10 21:20:38 +0000

    Updating pay and get to/from script scripts

--- getFromScript.sh
+++ getFromScript.sh
@@ -1,9 +1,10 @@
 source ./functions.sh
 
-PAYMENT=$1
-SCRIPT_NAME=${2}
+SCRIPT_NAME=${1}
 SCRIPT_FILE=./scripts/${SCRIPT_NAME}.plutus
-DATUM_VALUE=$3
+DATUM_VALUE=$2
+walletAddress $3
+TO_ADDR=$WALLET_ADDRESS
 if [ -z "$4" ]; then REDEEMER_VALUE=42; else REDEEMER_VALUE=$5; fi
 
 DATUM_HASH=$($CARDANO_CLI transaction hash-script-data --script-data-value "$DATUM_VALUE")
@@ -11,8 +12,9 @@ SCRIPT_ADDRESS=$($CARDANO_CLI address build --payment-script-file $SCRIPT_FILE -
 echo $SCRIPT_ADDRESS > ./wallets/${SCRIPT_NAME}.addr
 
 section "Select Script UTxO"
-getInputTx ${SCRIPT_NAME} $USER_WALLET
+getInputTx ${SCRIPT_NAME}
 SCRIPT_UTXO=$SELECTED_UTXO
+PAYMENT=$SELECTED_UTXO_LOVELACE
 
 section "Select Collateral UTxO"
 getInputTx fees
@@ -20,14 +22,12 @@ COLLATERAL_TX=$SELECTED_UTXO
 SIGNING_WALLET=$SELECTED_WALLET_NAME
 FEE_ADDR=$SELECTED_WALLET_ADDR
 
-walletAddress wallet1
-TO_ADDR=$WALLET_ADDRESS
-
 $CARDANO_CLI query protocol-parameters --testnet-magic $TESTNET_MAGIC_NUM > params.json
 
 removeTxFiles
 
 $CARDANO_CLI transaction build \
+--tx-in ${COLLATERAL_TX} \
 --tx-in ${SCRIPT_UTXO} \
 --tx-in-datum-value "${DATUM_VALUE}" \
 --tx-in-redeemer-value "${REDEEMER_VALUE}" \
--- payToScript.sh
+++ payToScript.sh
@@ -1,25 +1,23 @@
 source ./functions.sh
-getInputTx $5
+getInputTx $4
 
 FROM_ADDR=$SELECTED_WALLET_ADDR
 PAYMENT=$1
-FEE=$2
-CHANGE="$(($SELECTED_UTXO_LOVELACE-$PAYMENT-$FEE))"
-SCRIPT_ADDRESS=$($CARDANO_CLI address build --payment-script-file ./scripts/${3}.plutus --testnet-magic $TESTNET_MAGIC_NUM)
-DATUM_HASH=$($CARDANO_CLI transaction hash-script-data --script-data-value "$4")
+SCRIPT_ADDRESS=$($CARDANO_CLI address build --payment-script-file ./scripts/${2}.plutus --testnet-magic $TESTNET_MAGIC_NUM)
+DATUM_HASH=$($CARDANO_CLI transaction hash-script-data --script-data-value "$3")
 TO_ADDR=$SCRIPT_ADDRESS
 
-$CARDANO_CLI transaction build-raw \
+$CARDANO_CLI transaction build \
 --tx-in ${SELECTED_UTXO} \
 --tx-out ${TO_ADDR}+${PAYMENT} \
 --tx-out-datum-hash ${DATUM_HASH} \
---tx-out ${FROM_ADDR}+${CHANGE} \
---fee ${FEE} \
---out-file tx.raw \
+--change-address=${FROM_ADDR} \
+--testnet-magic $TESTNET_MAGIC_NUM \
+--out-file tx.build \
 --alonzo-era
 
 $CARDANO_CLI transaction sign \
---tx-body-file tx.raw \
+--tx-body-file tx.build \
 --signing-key-file ./wallets/${SELECTED_WALLET_NAME}.skey \
 --testnet-magic $TESTNET_MAGIC_NUM \
 --out-file tx.signed \
