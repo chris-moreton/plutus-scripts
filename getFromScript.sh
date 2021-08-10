6f2a240a2b4191ba0cbbc15d006a28c47df8f73d
Chris Moreton <chris@netsensia.com>
2021-08-10 20:56:03 +0000

    Use transaction build for getFromScript.sh

--- getFromScript.sh
+++ getFromScript.sh
@@ -1,62 +1,48 @@
 source ./functions.sh
 
 PAYMENT=$1
-FEE=$2
-SCRIPT_NAME=${3}
+SCRIPT_NAME=${2}
 SCRIPT_FILE=./scripts/${SCRIPT_NAME}.plutus
-DATUM_VALUE=$4
-if [ -z "$5" ]; then REDEEMER_VALUE=42; else REDEEMER_VALUE=$5; fi
+DATUM_VALUE=$3
+if [ -z "$4" ]; then REDEEMER_VALUE=42; else REDEEMER_VALUE=$5; fi
 
 DATUM_HASH=$($CARDANO_CLI transaction hash-script-data --script-data-value "$DATUM_VALUE")
 SCRIPT_ADDRESS=$($CARDANO_CLI address build --payment-script-file $SCRIPT_FILE --testnet-magic $TESTNET_MAGIC_NUM)
 echo $SCRIPT_ADDRESS > ./wallets/${SCRIPT_NAME}.addr
-SLOT=$(./currentSlot.sh)
 
 section "Select Script UTxO"
-getInputTx ${SCRIPT_NAME}
+getInputTx ${SCRIPT_NAME} $USER_WALLET
 SCRIPT_UTXO=$SELECTED_UTXO
-SCRIPT_CHANGE="$(($SELECTED_UTXO_LOVELACE-$PAYMENT))"
 
 section "Select Collateral UTxO"
 getInputTx fees
 COLLATERAL_TX=$SELECTED_UTXO
-FEE_UTXO=$SELECTED_UTXO
-FEE_CHANGE="$(($SELECTED_UTXO_LOVELACE-$FEE))"
-FEE_ADDR=$SELECTED_WALLET_ADDR
 SIGNING_WALLET=$SELECTED_WALLET_NAME
+FEE_ADDR=$SELECTED_WALLET_ADDR
 
-read -p 'Receiving Wallet: ' TO_WALLET_NAME
-walletAddress $TO_WALLET_NAME
+walletAddress wallet1
 TO_ADDR=$WALLET_ADDRESS
 
 $CARDANO_CLI query protocol-parameters --testnet-magic $TESTNET_MAGIC_NUM > params.json
 
 removeTxFiles
 
-if (($FEE_CHANGE <= 0)); then
-	echo "Fee UTxO too small"
-	exit
-fi
-
-$CARDANO_CLI transaction build-raw \
+$CARDANO_CLI transaction build \
 --tx-in ${SCRIPT_UTXO} \
 --tx-in-datum-value "${DATUM_VALUE}" \
 --tx-in-redeemer-value "${REDEEMER_VALUE}" \
 --tx-in-script-file $SCRIPT_FILE \
---tx-in-execution-units "(10000000000, 10000000000)" \
 --tx-in-collateral=${COLLATERAL_TX} \
---tx-in ${FEE_UTXO} \
---fee ${FEE} \
---tx-out ${FEE_ADDR}+${FEE_CHANGE} \
+--change-address=${FEE_ADDR} \
 --tx-out ${TO_ADDR}+${PAYMENT} \
---tx-out ${SCRIPT_ADDRESS}+${SCRIPT_CHANGE} \
 --tx-out-datum-hash ${DATUM_HASH} \
---out-file tx.raw \
+--out-file tx.build \
+--testnet-magic $TESTNET_MAGIC_NUM \
 --protocol-params-file "params.json" \
 --alonzo-era
 
 $CARDANO_CLI transaction sign \
---tx-body-file tx.raw \
+--tx-body-file tx.build \
 --signing-key-file ./wallets/${SIGNING_WALLET}.skey \
 --testnet-magic $TESTNET_MAGIC_NUM \
 --out-file tx.signed \
